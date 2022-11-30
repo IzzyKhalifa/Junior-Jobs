35e921868572a41ec57b1f317a7799e70c84318f
IzzyKhalifa <izzy.e.khalifa@gmail.com>
2022-11-30 20:03:31 +1100

    added job routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("../controllers/api/userRoutes");
 const userJobRoute = require("../controllers/api/userJobRoute");
+const jobRoute = require("../controllers/api/jobRoute");
 
 router.use("/api/users", userRoutes);
+router.use("/api/jobs", jobRoute);
 router.use("/api", userJobRoute);
 
 module.exports = router;
