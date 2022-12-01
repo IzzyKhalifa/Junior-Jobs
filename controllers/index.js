ebad78c829c304c9953a8437676f8b7ab2425dd5
IzzyKhalifa <izzy.e.khalifa@gmail.com>
2022-12-01 19:50:54 +1100

    Edited routes

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,8 @@ const userRoutes = require("../controllers/api/userRoutes");
 const userJobRoute = require("../controllers/api/userJobRoute");
 const jobRoute = require("../controllers/api/jobRoute");
 
-router.use("/api/users", userRoutes);
-router.use("/api/jobs", jobRoute);
-router.use("/api", userJobRoute);
+router.use("/users", userRoutes);
+router.use("/jobs", jobRoute);
+router.use("/", userJobRoute);
 
 module.exports = router;
